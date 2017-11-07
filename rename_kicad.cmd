6a3a002e80e8311a85d998d680124b8d823cd14e
raihun <raihun@raihusoft.jp>
2017-11-07 21:47:26 +0900

    Add rename_kicad.cmd

---
+++ rename_kicad.cmd
@@ -0,0 +1,11 @@
+@echo off
+set FILENAME=
+set /P FILENAME="ƒtƒ@ƒCƒ‹–¼‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢:"
+rename *.drl %FILENAME%.TXT
+rename *.gbl %FILENAME%.GBL
+rename *.gbs %FILENAME%.GBS
+rename *.gbo %FILENAME%.GBO
+rename *.gm1 %FILENAME%.GML
+rename *.gtl %FILENAME%.GTL
+rename *.gts %FILENAME%.GTS
+rename *.gto %FILENAME%.GTO
